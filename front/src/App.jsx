62cc7cbe8d66b553554638ef5e513de33683c862
Lou <Lou@constella.tech>
2023-09-20 13:57:25 -0600

    Initial Commit - Added all Ionic dependencies - Added Capacitor dependencies - Added React project - Added Rust Project

---
+++ front/src/App.jsx
@@ -0,0 +1,17 @@
+
+import {setupIonicReact, IonButton} from "@ionic/react";
+
+import '@ionic/react/css/core.css'
+setupIonicReact();
+
+function App() {
+
+
+  return (
+    <>
+    <IonButton>Code sex and crypto</IonButton>
+    </>
+  )
+}
+
+export default App
