4d4af3772200d8f7adb513ba759a0b28812860bc
Lou <Lou@constella.tech>
2023-09-22 01:35:13 -0600

    Added gear Templates

--- front/android/app/src/main/java/app/serenium/serenium/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package app.serenium.serenium;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ target/debug/deps/crypto_mexico_tij_23.d
@@ -0,0 +1,6 @@
+C:\code\cryptoMexicoTIJ2023\repo\crypto_mexico_tij_23\target\debug\deps\crypto_mexico_tij_23.exe: src\main.rs src\data_structures_module.rs
+
+C:\code\cryptoMexicoTIJ2023\repo\crypto_mexico_tij_23\target\debug\deps\crypto_mexico_tij_23.d: src\main.rs src\data_structures_module.rs
+
+src\main.rs:
+src\data_structures_module.rs:
