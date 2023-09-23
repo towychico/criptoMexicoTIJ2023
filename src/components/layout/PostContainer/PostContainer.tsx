1a9d91f38bae4000a351dec9119b5990b2a10ce4
Lou <Lou@constella.tech>
2023-09-22 20:41:37 -0600

    FixeFont for Top Bar and added Read me

--- src/components/layout/PostContainer/PostContainer.tsx
+++ src/components/layout/PostContainer/PostContainer.tsx
@@ -2,7 +2,10 @@ import react from "react"
 import './postContainer.css'
 import PostCard from "../PostCard/PostCard";
 
+
+
 function PostContainer(){
+
     return(<div className="postContainer">
         <PostCard title="What is your favorite pizza flavor?" content="Mine is Margarita, a timeless classic, delights with tangy tomatoes, creamy mozzarella, & fresh basil on crispy crust. Nostalgic & irresistible, it's a symphony of flavors! A culinary masterpiece! 🍕😍 " type={1}/>
         <PostCard title="I dare you to Watch a Scary Movie This Halloween! 🍿👻" content="👀 Are you up for it? Gather your bravest friends, grab some popcorn, and dive into a world of supernatural suspense. Share your movie picks and reactions" type={0}/>
